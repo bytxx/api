4a97a93233231bb7c55f951d36420ed062fa90d4
J. Alaiza <97946487+bytss@users.noreply.github.com>
2024-01-11 10:08:24 +0800

    Create etervpn.js

---
+++ etervpn.js
@@ -0,0 +1,20 @@
+{
+  "version": "1.0",
+  "message": "Hello World",
+  "servers": [
+    {
+      "name": "one",
+      "address": "test",
+      "port": 443,
+      "username": "test",
+      "password": "test"
+    }
+  ],
+  "networks": [
+    {
+      "name": "test",
+      "payload": "test",
+      "sni": "test"
+    }
+  ]
+}
